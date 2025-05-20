947cb29a2f60ee51db42c8514e9d5a22c9c0f858
PoletaevIlya <Ilya-poletae@mail.ru>
2025-05-20 14:15:03 +0700

    Realized Harvest Corn Particle + Small Fixes

--- Assets/Sources/Features/Player/Scripts/AbilitiesAnimationEvents.cs
+++ Assets/Sources/Features/Player/Scripts/AbilitiesAnimationEvents.cs
@@ -3,31 +3,11 @@ using UnityEngine;
 public class AbilitiesAnimationEvents : MonoBehaviour
 {
     [SerializeField] private ParticleSystem _seedParticle;
-    [SerializeField] private Transform _sickle;
-    [SerializeField] private LayerMask _mask;
+    [SerializeField] private HarvestAbility _harvestAbility;
 
     public void PlaySeedParticle() =>
         _seedParticle.Play();
 
-    public void ApplyHarvestOverlap()
-    {
-        Collider[] touchedColliders = Physics.OverlapSphere(_sickle.position, 1, _mask);
-
-        foreach (Collider touchedCollider in touchedColliders)
-        {
-            CropTile cropTile = touchedCollider.GetComponentInParent<CropTile>();
-
-            if (cropTile.IsEmpty == false && cropTile.IsWatered)
-                cropTile.Harvest();
-        }
-    }
-    
-    private void OnDrawGizmosSelected()
-    {
-        if (_sickle == null)
-            return;
-
-        Gizmos.color = Color.yellow;
-        Gizmos.DrawWireSphere(_sickle.position, 1);
-    }
+    public void ApplyHarvestOverlap() => 
+        _harvestAbility.ApplyHarvestOverlap();
 }
--- Assets/Sources/Features/Player/Scripts/HarvestAbility.cs
+++ Assets/Sources/Features/Player/Scripts/HarvestAbility.cs
@@ -3,10 +3,34 @@ using UnityEngine;
 public class HarvestAbility : MonoBehaviour, IAbility
 {
     [SerializeField] private PlayerAnimator _animator;
+    [SerializeField] private Transform _sickle;
+    [SerializeField] private LayerMask _mask;
     
     public void Apply() => 
         _animator.LaunchHarvestAnimation();
 
     public void Stop() => 
         _animator.StopHarvestAnimation();
+
+    public void ApplyHarvestOverlap()
+    {
+        Collider[] touchedColliders = Physics.OverlapSphere(_sickle.position, 1, _mask);
+
+        foreach (Collider touchedCollider in touchedColliders)
+        {
+            CropTile cropTile = touchedCollider.GetComponentInParent<CropTile>();
+
+            if (cropTile.IsEmpty == false && cropTile.IsWatered)
+                cropTile.Harvest();
+        }
+    }
+    
+    private void OnDrawGizmosSelected()
+    {
+        if (_sickle == null)
+            return;
+
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireSphere(_sickle.position, 1);
+    }
 }
